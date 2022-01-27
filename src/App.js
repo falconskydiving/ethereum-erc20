d90e7fed13ddaf7b6a7ec4200656c35517f94f6b
falconskydiving <falcon.skydiving.dev@gmail.com>
2022-01-26 18:25:59 -0800

    Create ERC20 token contract

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import Appauthenticated from "./components/AppAuthenticated";
+const Web3 = require("web3");
 
-function App() {
+const App = () => {
+  const [walletConnected, setWalletConnected] = useState(false);
+  const [instruction, setInstruction] = useState("Waiting for connection with wallet...");
+
+  useEffect(() => {
+    const connectWallet = async () => {
+      if(!window.ethereum)
+        return;
+
+      try {
+        await window.ethereum.send('eth_requestAccounts');
+        window.web3 = new Web3(window.ethereum);
+      } catch (error) {
+        setInstruction("Wallet connection denied, reload the page to try again.");
+        return;
+      }
+      setInstruction("");
+      setWalletConnected(true);
+    };
+    connectWallet();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {window.ethereum ?
+        (walletConnected ?
+          <Appauthenticated />
+          : instruction)
+        : "Metamask or other EIP-1102 / EIP-1193 compliant wallet not found."
+      }
     </div>
   );
 }
---
+++ src/components/AppAuthenticated.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Appauthenticated = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Appauthenticated;
